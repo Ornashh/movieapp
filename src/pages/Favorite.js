601a0d0d3ac4c0d5c9b4f69499a1ed6320441ac2
Ornash <ornash1337@gmail.com>
2021-08-16 19:27:55 +0500

    update favorite.js

--- src/pages/Favorite.js
+++ src/pages/Favorite.js
@@ -6,33 +6,41 @@ import { LazyLoadImage } from "react-lazy-load-image-component";
 
 import css from "./pages.module.scss";
 
-const { cards, cards_inner, cards_title, card } = css;
+const { cards, cards_inner, cards_title, card, center } = css;
 
 const Favorite = () => {
   const { favoriteArr, poster_img, posterNotFound } = useGlobalContext();
 
   return (
     <div className={`${cards} fade_in`}>
-      <div className={cards_title}>Favorite Movies</div>
-      <div className={cards_inner}>
-        {favoriteArr &&
-          favoriteArr.map((movie) => {
-            const { id, title, poster_path } = movie;
-            return (
-              <div key={id} className={card}>
-                <Link to={`/movie/${id}`}>
-                  <LazyLoadImage
-                    wrapperClassName="lazyLoad"
-                    src={
-                      poster_path ? poster_img + poster_path : posterNotFound
-                    }
-                    alt={title}
-                  />
-                </Link>
-              </div>
-            );
-          })}
-      </div>
+      {favoriteArr.length === 0 ? (
+        <div className={`${cards_title} ${center}`}>Favorite list is empty</div>
+      ) : (
+        <>
+          <div className={cards_title}>Favorite Movies</div>
+          <div className={cards_inner}>
+            {favoriteArr &&
+              favoriteArr.map((movie) => {
+                const { id, title, poster_path } = movie;
+                return (
+                  <div key={id} className={card}>
+                    <Link to={`/movie/${id}`}>
+                      <LazyLoadImage
+                        wrapperClassName="lazyLoad"
+                        src={
+                          poster_path
+                            ? poster_img + poster_path
+                            : posterNotFound
+                        }
+                        alt={title}
+                      />
+                    </Link>
+                  </div>
+                );
+              })}
+          </div>
+        </>
+      )}
     </div>
   );
 };
